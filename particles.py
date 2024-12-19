cfbaab17c9ed5b15836bbbdd5d246981d81e3427
Dennis Andersson <dennis.andersson106@gmail.com>
2024-12-19 03:21:14 +0100

    updated sand physics, ignore water

--- particles.py
+++ particles.py
@@ -23,6 +23,10 @@ class Particle():
     
     def isFalling(self):
         return self.falling
+    
+    def set_position(self, x, y):
+        self.x = x
+        self.y = y
 
 class Void(Particle):
     def __init__(self, x, y):
@@ -34,22 +38,55 @@ class Sand(Particle):
         self.color = (194,178,128)
         self.static = False
         self.solid = True
+        self.rand_color(1)
+
+    def rand_color(self, offset):
+        colors = [(246,215,176), (242,210,169), (236,204,162), (231,196,150), (225,191,146)]
+        self.color = random.choice(colors)
+        
+    def set_color(self, color):
+        self.color = color
 
     def update(self, world, newWorld, x, y):
+        cols = len(world)
         rows = len(world[0])
         if y == rows - 1: # if at bottom
-            newWorld[x][y] = Sand(x, y)
+            s = Sand(x, y)
+            s.set_color(self.color)
+            newWorld[x][y] = s
             return newWorld
         
         belowParticle = world[x][y+1]
+        belowParticleNew = newWorld[x][y+1]
 
         if isinstance(belowParticle, Void):
             newWorld[x][y+1] = Sand(x, y+1)
         elif belowParticle.isSolid():
-            newWorld[x][y] = Sand(x, y)
-        elif isinstance(belowParticle, Water):
+            s = Sand(x, y)
+            s.set_color(self.color)
+
+            left = x-1 > 0 and isinstance(world[x-1][y], Void)
+            right = x+1 < cols - 1 and isinstance(world[x+1][y], Void)
+            down_left = left and isinstance(world[x-1][y+1], Void)
+            down_right = right and isinstance(world[x+1][y+1], Void)
+            if down_left or down_right:
+                if down_left and down_left:
+                    spread = random.choice([(x-1, y+1), (x+1, y+1)])
+                    _x, _y = spread
+                    s.set_position(_x, _y)
+                    newWorld[_x][_y] = s
+                elif down_left:
+                    s.set_position(x-1, y+1)
+                    newWorld[x-1][y+1] = s
+                elif down_right:
+                    s.set_position(x+1, y+1)
+                    newWorld[x+1][y+1] = s
+                return newWorld
+            newWorld[x][y] = s
+        elif isinstance(belowParticle, Water) and isinstance(belowParticleNew, Water):
             newWorld[x][y+1] = Sand(x, y+1)
             newWorld[x][y] = Water(x, y)
+            pass
         return newWorld
 
 class Water(Particle):
@@ -62,25 +99,26 @@ class Water(Particle):
     def update(self, world, newWorld, x, y):
         cols = len(world)
         rows = len(world[0])
-        if y == rows - 1: # if at bottom
+        if y == rows - 1: 
             newWorld[x][y] = Water(x, y)
             return newWorld
         
         belowParticle = world[x][y+1]
+        belowParticleNew = newWorld[x][y+1]
 
-        #if on void
         if isinstance(belowParticle, Void):
             newWorld[x][y+1] = Water(x, y+1)
             return newWorld
+        
+        if isinstance(belowParticle, Water) and isinstance(belowParticleNew, Void):
+            newWorld[x][y+1] = Water(x, y+1)
+            return newWorld
 
-        # if on solid
         if belowParticle.isSolid():
-            # no collisions
             left = x-1 > 0 and isinstance(world[x-1][y], Void)
             right = x+1 < cols - 1 and isinstance(world[x+1][y], Void)
             down_left = left and isinstance(world[x-1][y+1], Void)
             down_right = right and isinstance(world[x+1][y+1], Void)
-            # if on hill spread randomly
             if down_left or down_right:
                 if down_left and down_left:
                     spread = random.choice([(x-1, y+1), (x+1, y+1)])
@@ -90,6 +128,17 @@ class Water(Particle):
                     newWorld[x-1][y+1] = Water(x-1, y+1)
                 elif down_right:
                     newWorld[x+1][y+1] = Water(x+1, y+1)
+                return newWorld
+            else:
+                if left and right:
+                    spread = random.choice([(x-1, y), (x+1, y)])
+                    _x, _y = spread
+                    newWorld[_x][_y] = Water(_x, _y)
+                elif left:
+                    newWorld[x-1][y] = Water(x-1, y)
+                elif right:
+                    newWorld[x+1][y] = Water(x+1, y)
+                return newWorld
         newWorld[x][y] = Water(x,y)
         return newWorld
     
